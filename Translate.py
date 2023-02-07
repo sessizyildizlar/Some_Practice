4970856cffe581c5a9f66cc3d5f27cf76ac7438a
AloneStars <122230149+sessizyildizlar@users.noreply.github.com>
2023-02-07 20:40:33 +0300

    Translate

---
+++ Translate.py
@@ -0,0 +1,6 @@
+EnglishtoTurkish={"car":"","cloud":"","sun":"","rain":"","nephew":""}
+dictionary={}
+for key in EnglishtoTurkish.keys():
+    dictionary[key]=input(f"Please,Could you write what is meaning of {key}? :")
+
+print(dictionary) 
